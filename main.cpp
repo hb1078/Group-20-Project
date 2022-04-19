f6d8eb0bc5d045a43667ce71151ea7fc2cc1698c
hb1078 <harisebalamurugan@gmail.com>
2022-04-18 22:19:09 -0500

    creation of a test main.cpp file

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << ("test for source control") << endl;
+    cin >> n;
+    cout << n << endl;
+}
+
+
