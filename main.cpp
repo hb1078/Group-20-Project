9c2cb12549079f6b96144e184c4f109d87463cab
hb1078 <harisebalamurugan@gmail.com>
2022-04-18 22:30:50 -0500

    slight control to see changes

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main()
 {
     int n;
-    cout << ("test for source control") << endl;
+    cout << ("test for source control v2") << endl;
     cin >> n;
     cout << n << endl;
 }
