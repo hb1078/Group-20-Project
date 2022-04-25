faa3b4dec8e0e10cd8085c831dd4ded88f0f42ad
hb1078 <harisebalamurugan@gmail.com>
2022-04-25 18:43:05 -0500

    trying to get this to work has updates, but won't build -harish

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ int main()
 {
     //variables for main's usage
     int user_input;
+    int login_option;
 
     string line;
     string fullnametemp;
@@ -35,6 +36,31 @@ int main()
 
     file1.open(accountsfile);
 
+    //reads in account information from accounts.txt
+    if(file1.is_open()){
+        while(getline(file1, line)){
+            accounts fileaccount;
+            fileaccount.setFullName(line);
+
+            getline(file1, line);
+            fileaccount.setPassword(line);
+
+            getline(file1, line);
+            fileaccount.setShippingInfo(line);
+
+            getline(file1, line);
+            fileaccount.setPaymentInfo(line);
+
+            account_vector.push_back(fileaccount);
+        }
+
+        file1.close();
+
+    }
+    else{
+        cout << "Error opening file" << endl;
+    }
+
     //main menu
     cout << "Welcome to the Group 20 Bookstore!" << endl;
     cout << "Please select from the following options:" << endl << endl;
@@ -47,15 +73,39 @@ int main()
     cin >> user_input;
 
     //acc login (must have created account OR already have one in the .txt file)
-    if(user_input == 1)
+    if(user_input == 1) 
     {
+        cout << "---User Login---" << endl << endl;
+        cout << "Please enter the name you registered with: ";
+        getline(cin >> ws, fullnametemp);
 
+        for(int i = 0; i < account_vector.size(); i++) {          
+                  //THIS IS GOING TO BE A MASSIVE FOR-LOOP (it'll be the entire login menu)
+            if(fullnametemp == account_vector[i].getFullName()) {
+
+                cout << "Please enter your password: ";
+                getline(cin, passwordtemp);
+
+                if(passwordtemp == account_vector[i].getPassword()) {
+
+                    cout << "Welcome " << fullnametemp << "!" << endl;
+                    cout << "Please select from the following options:" << endl << endl;
+
+                    cout << "1. View Shop" << endl;
+                    cout << "2. View Cart" << endl;
+                    cout << "3. View Account" << endl;
+                    cout << "4. Logout" << endl;
+
+                    cin >> login_option;
+                }
+            }
+        }
     }
 
     //account creation
     else if(user_input == 2)
     {
-        cout << "--- User Account Creation --- " << endl;
+        cout << "--- User Account Creation --- " << endl << endl;
 
         cout << "Please enter your first and last name: " << endl;
         getline(cin >> ws, fullnametemp);
@@ -69,6 +119,10 @@ int main()
         cout << "Please enter your card number:" << endl;
         getline(cin, paymentinfotemp);
 
+        accounts newaccount(fullnametemp, passwordtemp, shippinginfotemp, paymentinfotemp);
+        account_vector.push_back(newaccount);
+
+        cout << "Account created successfully!" << endl;
 
     }
 
