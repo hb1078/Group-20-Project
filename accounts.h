c319813724c6fca27c22a041f1e17095dda66fd6
mtswip <samuelhorn0@gmail.com>
2022-04-27 19:57:58 -0500

    changed accounts.cpp to where I can get it to work. Fix required moving cpp code back, and moving includes to .h file

--- accounts.h
+++ accounts.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iostream>
 #include <string>
 
 using namespace std;
@@ -39,56 +40,3 @@ class accounts
     {};
 
 };
-
-
-////////////////////////////////////////////////// FORMER ACCOUNTS.CPP STARTS HERE ////////////////////////////////////////////////////////////////////////////
-
-void accounts::setFullName(string fullname)
-{
-    this->fullname = fullname;
-}
-
-void accounts::setPassword(string password)
-{
-    this->password = password;
-}
-
-void accounts::setShippingInfo(string shippingInfo)
-{
-    this->shippingInfo = shippingInfo;
-}
-
-void accounts::setPaymentInfo(string paymentInfo)
-{
-    this->paymentInfo = paymentInfo;
-}
-
-void accounts::getUserInfo()
-{
-    cout << "Full Name: " << fullname << endl;
-    cout << "Password: " << password << endl;
-    cout << "Shipping Info: " << shippingInfo << endl;
-    cout << "Payment Info: " << paymentInfo << endl;
-}
-
-string accounts::getFullName()
-{
-    return fullname;
-}
-
-string accounts::getPassword()
-{
-    return password;
-}
-
-string accounts::getShippingInfo()
-{
-    return shippingInfo;
-}
-
-string accounts::getPaymentInfo()
-{
-    return paymentInfo;
-}
-
-//2 CART FUNCTIONS (clear/view) GO HERE, RECONSIDER IF THIS IS NEEDED IN ACCOUNTS.CPP OR NOT
