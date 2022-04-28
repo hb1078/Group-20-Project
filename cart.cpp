1a60f41b4e8fea08d66dd5ea839122aadd69c9f1
Ethan Bullock <43189438+ekbullock@users.noreply.github.com>
2022-04-27 20:13:50 -0500

    Add files via upload

--- cart.cpp
+++ cart.cpp
@@ -1,1 +1,41 @@
-cart.cpp
+#include "cart.h> 
+#include <iostream>
+
+using namespace std;
+
+void cart::setTotal(double totalIN)
+{
+    setTotal = setTotalIn;
+}
+
+void cart::getTotal(double TotalIN)
+{
+    getTotal = getTotalIn;
+}
+
+string cart::getUsername()
+{
+    return Username;
+}
+
+string cart::getBookName()
+{
+    return Bookname;
+}
+
+double cart::getBookPrice(double BookPrice2)
+{
+    getBookPrice = bookPrice2;
+}
+
+void cart::addBook(Book book) //going by design doc
+{
+;
+}
+
+void cart::removeBook(Book book)
+{
+    ;
+}
+
+void
